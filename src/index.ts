bdcd4a62cce711fbfb91f125d1b1320e7402b46e
William Harrison <william@williamdavidharrison.com.au>
2023-10-21 13:42:16 +0800

    feat: update status

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const client = new ExtendedClient({
     presence: {
         activities: [
             {
-                name: "wdh.gg » !help",
+                name: "Managing servers » /help",
                 type: Discord.ActivityType.Custom
             }
         ],
