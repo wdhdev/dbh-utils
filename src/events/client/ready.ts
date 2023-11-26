65161c51ea5cb1f99b09227702db103788ac77be
William Harrison <william@williamdavidharrison.com.au>
2023-11-26 15:01:35 +0800

    feat: git pull

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -13,6 +13,17 @@ const event: Event = {
 
             // Register Commands
             await globalCommands(client);
+
+            // Automatic Git Pull
+            setInterval(() => {
+                exec("git pull", (err: any, stdout: any) => {
+                    if(err) return console.log(err);
+                    if(stdout.includes("Already up to date.")) return;
+
+                    console.log(stdout);
+                    process.exit();
+                })
+            }, 30 * 1000) // 30 seconds
         } catch(err) {
             client.logError(err);
         }
